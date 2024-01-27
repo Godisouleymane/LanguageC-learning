91b673d6554236774e3b57f0cae352bd03670fdc
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-27 23:53:59 +0000

    les differents types des variables

--- variable/variable.c
+++ variable/variable.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
-	printf("Bienvenue sur notre programme");
+	int nmbreEntier;
+	float nmbreDecimal = 9.8;
+	char caractere;
+	
+	nmbreEntier = 42; // affectation de 42 a nombre entier;
+	caractere = 'a';
+	printf("Bienvenue sur notre programme \n");
+	printf("Un entier => %d \n", nmbreEntier);
+	printf("Un decimal => %f \n", nmbreDecimal);
+	printf("Un caractere => %c \n", caractere);
 	return 0;
 }
