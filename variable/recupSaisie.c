6855422643869c5670735cdf8cb9ff43f6276a5a
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-30 01:02:55 +0000

    recuperer une valeur saisie par le user

---
+++ variable/recupSaisie.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int age = 0;
+	printf("Quel age avez-vous ?");
+	scanf("%d", &age);
+	printf("Ah vous avez donc %d ans \n\n", age);
+	return 0;
+}
