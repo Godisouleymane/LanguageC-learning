b7389560c0872c086c4463fe4f2815a490cebcb0
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-30 00:38:32 +0000

    les variables

--- variable/variable2.c
+++ variable/variable2.c
@@ -2,11 +2,11 @@
 #include<stdlib.h>
 int main(int argc, char * argv[])
 {
-	int nombreDeVies = 5;
+	int nombreDeVies = 5, niveau = 1;
 	printf("Vous avez %d vies\n", nombreDeVies);
 	printf("***** B A M *****\n");
 	nombreDeVies = 4;
-	printf("Il ne vous reste plus que %d vies maintenant\n\n", nombreDeVies);
+	printf("Il ne vous reste plus que %d vies maintenant, et vous etes au niveau %d", nombreDeVies, niveau);
 	return 0;
 }
 
