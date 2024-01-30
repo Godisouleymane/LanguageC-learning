e46ee7bb40ea137f2b78c29ea3176d98a69a5f4a
GodiSouleymane <ssabiougodi@gmail.com>
2024-01-30 18:37:36 +0000

    les conditions

---
+++ lesConditions/condition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int age = 20;
+	
+	// verifier si age est superieure ou egal a 18;
+
+	if(age >= 18){
+		printf("Ah vous etes majeur");
+	}
+
+	return 0;
+}
